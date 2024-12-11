66971ef3a212388999c514ad6d9f7e29bde95d37
Chauhankhushab <harshchauhan08866@gmail.com>
2024-12-11 18:28:41 +0530

    Add files via upload

---
+++ Python/Python Fundamentals/labs program/q2.py
@@ -0,0 +1,19 @@
+#  Set up Python on your local machine and write a program to display your name
+
+# stepp:-1
+
+# -> python3 --version
+
+# step:-2
+
+# ->python file_name.py
+
+# step:-3
+
+# print("chauhan khushab")
+
+# example
+
+a=str(input("ENTER YOUR NAME:-"))
+
+print("Name:-",a)
